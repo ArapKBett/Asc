408b195d3c86aada8fb8f7927c31128e96df49f5
Arap Bett <kipkorirjob@kabarak.ac.ke>
2025-04-10 07:57:55 -0400

    Initial commit of SecureChain Forensics project

--- backend/crypto.cpp
+++ backend/crypto.cpp
@@ -1,47 +1,94 @@
 #include "crypto.h"
 #include <fstream>
-#include <openssl/aes.h>
+#include <openssl/evp.h>
 #include <openssl/rand.h>
 #include <iostream>
+#include <cstring>
 
 const std::string Crypto::key = "12345678901234567890123456789012";  // 32 bytes
 const std::string Crypto::iv = "abcdefghijklmnop";                  // 16 bytes
 
 void Crypto::encryptFile(const std::string& inputFile, const std::string& outputFile) {
-    unsigned char iv[AES_BLOCK_SIZE];
-    memcpy(iv, Crypto::iv.c_str(), AES_BLOCK_SIZE);
+    EVP_CIPHER_CTX* ctx = EVP_CIPHER_CTX_new();
+    if (!ctx) {
+        std::cerr << "Failed to create EVP context\n";
+        return;
+    }
+
+    unsigned char iv[Crypto::AES_BLOCK_SIZE];
+    memcpy(iv, Crypto::iv.c_str(), Crypto::AES_BLOCK_SIZE);
 
     std::ifstream inFile(inputFile, std::ios::binary);
     std::ofstream outFile(outputFile, std::ios::binary);
-    outFile.write((char*)iv, AES_BLOCK_SIZE);  // Write IV
+    outFile.write((char*)iv, Crypto::AES_BLOCK_SIZE);  // Write IV
 
-    AES_KEY aesKey;
-    AES_set_encrypt_key((unsigned char*)key.c_str(), 256, &aesKey);
-
-    unsigned char inBuf[AES_BLOCK_SIZE], outBuf[AES_BLOCK_SIZE];
-    while (inFile.read((char*)inBuf, AES_BLOCK_SIZE) || inFile.gcount()) {
-        int bytesRead = inFile.gcount();
-        AES_cbc_encrypt(inBuf, outBuf, bytesRead, &aesKey, iv, AES_ENCRYPT);
-        outFile.write((char*)outBuf, bytesRead);
+    if (EVP_EncryptInit_ex(ctx, EVP_aes_256_cbc(), NULL, (unsigned char*)key.c_str(), iv) != 1) {
+        std::cerr << "Failed to initialize encryption\n";
+        EVP_CIPHER_CTX_free(ctx);
+        return;
     }
+
+    unsigned char inBuf[1024], outBuf[1024 + Crypto::AES_BLOCK_SIZE];
+    int outLen;
+    while (inFile.read((char*)inBuf, sizeof(inBuf)) || inFile.gcount()) {
+        int bytesRead = inFile.gcount();
+        if (EVP_EncryptUpdate(ctx, outBuf, &outLen, inBuf, bytesRead) != 1) {
+            std::cerr << "Encryption update failed\n";
+            EVP_CIPHER_CTX_free(ctx);
+            return;
+        }
+        outFile.write((char*)outBuf, outLen);
+    }
+
+    if (EVP_EncryptFinal_ex(ctx, outBuf, &outLen) != 1) {
+        std::cerr << "Encryption finalization failed\n";
+        EVP_CIPHER_CTX_free(ctx);
+        return;
+    }
+    outFile.write((char*)outBuf, outLen);
+
+    EVP_CIPHER_CTX_free(ctx);
 }
 
 void Crypto::decryptFile(const std::string& inputFile, const std::string& outputFile) {
+    EVP_CIPHER_CTX* ctx = EVP_CIPHER_CTX_new();
+    if (!ctx) {
+        std::cerr << "Failed to create EVP context\n";
+        return;
+    }
+
     std::ifstream inFile(inputFile, std::ios::binary);
     std::ofstream outFile(outputFile, std::ios::binary);
 
-    unsigned char iv[AES_BLOCK_SIZE];
-    inFile.read((char*)iv, AES_BLOCK_SIZE);
+    unsigned char iv[Crypto::AES_BLOCK_SIZE];
+    inFile.read((char*)iv, Crypto::AES_BLOCK_SIZE);
 
-    AES_KEY aesKey;
-    AES_set_decrypt_key((unsigned char*)key.c_str(), 256, &aesKey);
-
-    unsigned char inBuf[AES_BLOCK_SIZE], outBuf[AES_BLOCK_SIZE];
-    while (inFile.read((char*)inBuf, AES_BLOCK_SIZE) || inFile.gcount()) {
-        int bytesRead = inFile.gcount();
-        AES_cbc_encrypt(inBuf, outBuf, bytesRead, &aesKey, iv, AES_DECRYPT);
-        outFile.write((char*)outBuf, bytesRead);
+    if (EVP_DecryptInit_ex(ctx, EVP_aes_256_cbc(), NULL, (unsigned char*)key.c_str(), iv) != 1) {
+        std::cerr << "Failed to initialize decryption\n";
+        EVP_CIPHER_CTX_free(ctx);
+        return;
     }
+
+    unsigned char inBuf[1024], outBuf[1024 + Crypto::AES_BLOCK_SIZE];
+    int outLen;
+    while (inFile.read((char*)inBuf, sizeof(inBuf)) || inFile.gcount()) {
+        int bytesRead = inFile.gcount();
+        if (EVP_DecryptUpdate(ctx, outBuf, &outLen, inBuf, bytesRead) != 1) {
+            std::cerr << "Decryption update failed\n";
+            EVP_CIPHER_CTX_free(ctx);
+            return;
+        }
+        outFile.write((char*)outBuf, outLen);
+    }
+
+    if (EVP_DecryptFinal_ex(ctx, outBuf, &outLen) != 1) {
+        std::cerr << "Decryption finalization failed\n";
+        EVP_CIPHER_CTX_free(ctx);
+        return;
+    }
+    outFile.write((char*)outBuf, outLen);
+
+    EVP_CIPHER_CTX_free(ctx);
 }
 
 int main(int argc, char* argv[]) {
--- backend/crypto.h
+++ backend/crypto.h
@@ -8,8 +8,9 @@ public:
     static void encryptFile(const std::string& inputFile, const std::string& outputFile);
     static void decryptFile(const std::string& inputFile, const std::string& outputFile);
 private:
-    static const std::string key;  // AES-256 key (32 bytes)
-    static const std::string iv;   // Initialization vector (16 bytes)
+    static const std::string key;  // 32 bytes for AES-256
+    static const std::string iv;   // 16 bytes for CBC
+    static const int AES_BLOCK_SIZE = 16;  // Define block size explicitly
 };
 
 #endif
